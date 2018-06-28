3de4c779cc1958a40ac60247e82557efe0cdca40
pieter <pieter@pool.zoin.netabuse.net>
2018-06-28 15:54:38 +0200

    initial

---
+++ build/Release/.deps/Release/obj.target/multihashing/boolberry.o.d
@@ -0,0 +1,12 @@
+cmd_Release/obj.target/multihashing/boolberry.o := g++ '-DNODE_GYP_MODULE_NAME=multihashing' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pieter/.node-gyp/8.11.3/include/node -I/home/pieter/.node-gyp/8.11.3/src -I/home/pieter/.node-gyp/8.11.3/deps/openssl/config -I/home/pieter/.node-gyp/8.11.3/deps/openssl/openssl/include -I/home/pieter/.node-gyp/8.11.3/deps/uv/include -I/home/pieter/.node-gyp/8.11.3/deps/zlib -I/home/pieter/.node-gyp/8.11.3/deps/v8/include -I../crypto -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -std=c++11 -MMD -MF ./Release/.deps/Release/obj.target/multihashing/boolberry.o.d.raw   -c -o Release/obj.target/multihashing/boolberry.o ../boolberry.cc
+Release/obj.target/multihashing/boolberry.o: ../boolberry.cc \
+ ../boolberry.h ../crypto/cryptonote_core/cryptonote_format_utils.h \
+ ../crypto/cryptonote_core/../hash.h \
+ ../crypto/cryptonote_core/../hash-ops.h \
+ ../crypto/cryptonote_core/../wild_keccak.h
+../boolberry.cc:
+../boolberry.h:
+../crypto/cryptonote_core/cryptonote_format_utils.h:
+../crypto/cryptonote_core/../hash.h:
+../crypto/cryptonote_core/../hash-ops.h:
+../crypto/cryptonote_core/../wild_keccak.h:
---
+++ build/Release/.deps/Release/obj.target/multihashing/multihashing.o.d
@@ -0,0 +1,102 @@
+cmd_Release/obj.target/multihashing/multihashing.o := g++ '-DNODE_GYP_MODULE_NAME=multihashing' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/pieter/.node-gyp/8.11.3/include/node -I/home/pieter/.node-gyp/8.11.3/src -I/home/pieter/.node-gyp/8.11.3/deps/openssl/config -I/home/pieter/.node-gyp/8.11.3/deps/openssl/openssl/include -I/home/pieter/.node-gyp/8.11.3/deps/uv/include -I/home/pieter/.node-gyp/8.11.3/deps/zlib -I/home/pieter/.node-gyp/8.11.3/deps/v8/include -I../crypto -I../../nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++0x -std=c++11 -MMD -MF ./Release/.deps/Release/obj.target/multihashing/multihashing.o.d.raw   -c -o Release/obj.target/multihashing/multihashing.o ../multihashing.cc
+Release/obj.target/multihashing/multihashing.o: ../multihashing.cc \
+ /home/pieter/.node-gyp/8.11.3/include/node/node.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/v8.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/v8-version.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/v8config.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/node_version.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/node_buffer.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/node.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/v8.h ../../nan/nan.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/node_version.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv-errno.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv-version.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv-unix.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv-threadpool.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/uv-linux.h \
+ /home/pieter/.node-gyp/8.11.3/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h ../../nan/nan_private.h \
+ ../../nan/nan_typedarray_contents.h ../../nan/nan_json.h ../argon2.h \
+ ../bcrypt.h ../blake.h ../c11.h ../cryptonight.h ../dcrypt.h ../fresh.h \
+ ../fugue.h ../groestl.h ../hefty1.h ../jh.h ../keccak.h ../lyra2.h \
+ ../lyra2re.h ../lyra2z.h ../lyra2z330.h ../neoscrypt.h ../nist5.h \
+ ../quark.h ../qubit.h ../s3.h ../scryptjane.h ../scryptn.h ../sha1.h \
+ ../shavite3.h ../skein.h ../Sponge.h ../tribus.h ../whirlpoolx.h ../x5.h \
+ ../x11.h ../x11ghost.h ../x13.h ../x14.h ../x15.h ../x16r.h ../zr5.h \
+ ../yescrypt/yescrypt.h ../yescrypt/sha256_Y.h ../boolberry.h
+../multihashing.cc:
+/home/pieter/.node-gyp/8.11.3/include/node/node.h:
+/home/pieter/.node-gyp/8.11.3/include/node/v8.h:
+/home/pieter/.node-gyp/8.11.3/include/node/v8-version.h:
+/home/pieter/.node-gyp/8.11.3/include/node/v8config.h:
+/home/pieter/.node-gyp/8.11.3/include/node/node_version.h:
+/home/pieter/.node-gyp/8.11.3/include/node/node_buffer.h:
+/home/pieter/.node-gyp/8.11.3/include/node/node.h:
+/home/pieter/.node-gyp/8.11.3/include/node/v8.h:
+../../nan/nan.h:
+/home/pieter/.node-gyp/8.11.3/include/node/node_version.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv-errno.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv-version.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv-unix.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv-threadpool.h:
+/home/pieter/.node-gyp/8.11.3/include/node/uv-linux.h:
+/home/pieter/.node-gyp/8.11.3/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_private.h:
+../../nan/nan_typedarray_contents.h:
+../../nan/nan_json.h:
+../argon2.h:
+../bcrypt.h:
+../blake.h:
+../c11.h:
+../cryptonight.h:
+../dcrypt.h:
+../fresh.h:
+../fugue.h:
+../groestl.h:
+../hefty1.h:
+../jh.h:
+../keccak.h:
+../lyra2.h:
+../lyra2re.h:
+../lyra2z.h:
+../lyra2z330.h:
+../neoscrypt.h:
+../nist5.h:
+../quark.h:
+../qubit.h:
+../s3.h:
+../scryptjane.h:
+../scryptn.h:
+../sha1.h:
+../shavite3.h:
+../skein.h:
+../Sponge.h:
+../tribus.h:
+../whirlpoolx.h:
+../x5.h:
+../x11.h:
+../x11ghost.h:
+../x13.h:
+../x14.h:
+../x15.h:
+../x16r.h:
+../zr5.h:
+../yescrypt/yescrypt.h:
+../yescrypt/sha256_Y.h:
+../boolberry.h:
